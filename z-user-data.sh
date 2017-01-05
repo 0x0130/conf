473fc9cb872424daec91656a553d75171987df6b
0x0130 <ok0x0130@gmail.com>
2017-01-05 18:13:44 +0900

    Update z-user-data.sh

--- z-user-data.sh
+++ z-user-data.sh
@@ -46,7 +46,4 @@ VimBackup() { local _bak="$1-$(date +%Y%m%d-%H%M%S).bak"; [ -f "$1" ] && cp -pi 
 alias vim='VimBackup'
 alias vi='VimBackup'
 
-GetAwsRegion() { echo $(curl -s --connect-timeout 2 http://169.254.169.254/latest/meta-data/placement/availability-zone); }
-alias get-aws-region='GetAwsRegion'
-
 #EOF
